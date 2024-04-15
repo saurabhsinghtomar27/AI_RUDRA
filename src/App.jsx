aa8aa85c038bd617c8b4ccb529720470d50a3b31
saurabhsinghtomar27 <saurabhsingh27feb2003@gmail.com>
2024-04-15 18:58:08 +0530

    finalazing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import ImageGenerator from './Components/ImageGenerator'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    <ImageGenerator />
+    </>
+  )
+}
+
+export default App
