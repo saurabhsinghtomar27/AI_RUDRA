868a1a0b39c4c8f6f854e937c7545c20df27bad5
saurabhsinghtomar27 <saurabhsingh27feb2003@gmail.com>
2024-04-16 00:03:21 +0530

    copy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MEET OUR RUDRA AND TALK WITH HIM
-## LINK - [AI_RUDRA](www.google.com)  
+## LINK - [AI_RUDRA](https://rudraai.netlify.app/)  
+
+![image](https://github.com/saurabhsinghtomar27/AI_RUDRA/assets/94640771/16d4e1c2-87e2-435e-90a0-b7780e33291b)
--- src/Components/ImageGenerator.jsx
+++ src/Components/ImageGenerator.jsx
@@ -2,19 +2,24 @@ import React, { useState } from 'react'
 import { GoogleGenerativeAI,GenerativeModel  } from '@google/generative-ai';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import {API_KEY} from './API.js'
 function ImageGenerator() {
     const [text,settext]=useState('')
     const [input,setinput]=useState('')
    const [tf,settf]=useState(true)
+   const copy=()=>{
+    navigator.clipboard.writeText(text);
+    toast.success("Copied to Clipboard")
+  }
     const generateImage=async(e)=>{
         e.preventDefault();
        if(!input)
        {
-        toast.error("Promt is Empty !")
+        toast.error("Prompt is Empty !")
          return
        }
-      
         try {
           settf(false)
           const apiKey = API_KEY
@@ -22,34 +27,49 @@ function ImageGenerator() {
           const model = genAI.getGenerativeModel({ model: "gemini-pro"});
           const response = await model.generateContent(input);
           settf(true)
+          settext((prev)=> prev+"\nYou - "+input+"\n"+"RUDRA  \n"+response.response.candidates[0].content.parts[0].text)
+          // settext((prev)=>prev+"\nYou -"+input+"\n"+"RUDRA  '\n"+respone)
           setinput('')
-          settext((prev)=> prev+"\n"+response.response.candidates[0].content.parts[0].text)
     }
          catch (error) {
           console.log("error= "+error);
         }
       };
-    
+      
   return (
     <>
     <div className='justify-center m-12'>
       <div>
     <h1 className='text-3xl font-bold py-1 flex'> <span className='text-white'>AI </span><span className='underline decoration-sky-500 text-red-600 px-2'>RUDRA</span></h1>
-    <div className='flex'>
+    <div className=''>
+      <div>
+      <CodeMirror
+      value={text}
+      theme={vscodeDark}
+      height="500px"
+      width='800px'
+    />
+    <button className='ml-2 px-7 py-2 border-solid border-2 border-blue-500 bg-blue-700 hover:bg-blue-400 text-slate-200 rounded-full' disabled ={(text.length==0)?true:false} onClick={()=>settext("")}>Clear</button>
+      <button className='ml-2 px-7 py-2 border-solid border-2 border-lime-700 bg-lime-600 hover:bg-lime-500 text-slate-200 rounded-full' disabled ={(text.length==0)?true:false} onClick={copy}>Copy</button>
         <form onSubmit={generateImage} >
         <div className="w-96 py-4">
   <div className="relative w-full min-w-[200px]">
-    <textarea 
-      value={text}
-      className="flex textown peer h-full min-h-[500px] resize rounded-[7px] border border-blue-gray-200 border-t-transparent bg-black px-3 py-2.5 font-sans text-xl font-normal text-slate-300 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-      placeholder="Your Response"></textarea>
+  
+    <div className=''>
+  
+     
+     </div>
   </div>
+  
 </div>
     <input className='w-96 py-2 px-2 bg-black text-slate-200 rounded-full' type='text' placeholder='Enter a Promt' value={input} onChange={(e)=>setinput(e.target.value)} />
     <button className='ml-2 px-7 py-2 border-solid border-2 border-red-500 bg-red-700 hover:bg-red-400 text-slate-200 rounded-full'>{tf==false?'Generating...':'Generate'}</button>
-    <button className='ml-2 px-7 py-2 border-solid border-2 border-blue-500 bg-blue-700 hover:bg-blue-400 text-slate-200 rounded-full'  onClick={()=>settext("")}>Clear</button>
+    
+    
     </form> 
+    
     <div>
+    </div>
     </div>
 </div>
 </div>
